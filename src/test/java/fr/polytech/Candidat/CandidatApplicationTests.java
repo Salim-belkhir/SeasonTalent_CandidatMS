8311f5e9d7ecbf791292439aaf393c9bbbecb949
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-10-08 19:16:29 +0200

    Creation of the Micro Service

---
+++ src/test/java/fr/polytech/Candidat/CandidatApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.Candidat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
